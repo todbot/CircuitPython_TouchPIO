1e3759acc9bb5c48b27b84fda0c6062a63a9336d
Tod Kurt <todbot@users.noreply.github.com>
2023-08-24 13:13:07 -0700

    first

---
+++ docs/examples.rst
@@ -0,0 +1,8 @@
+Simple test
+------------
+
+Ensure your device works with this simple test.
+
+.. literalinclude:: ../examples/touchpio_simpletest.py
+    :caption: examples/touchpio_simpletest.py
+    :linenos:
---
+++ touchpio.py
@@ -0,0 +1,122 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2023 Tod Kurt
+#
+# SPDX-License-Identifier: MIT
+"""
+`touchpio`
+================================================================================
+
+Capacitive touch sensing using Pico / RP2040 PIO, using touchio API
+
+
+* Author(s): Tod Kurt
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+  based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+
+**References:**
+
+ * Uses ideas from `PIO Capsense experiment / -scottiebabe 2022 <https://community.element14.com/products/raspberry-pi/f/forum/51242/want-to-create-a-capacitance-proximity-touch-sensor-with-a-rp2040-pico-board-using-pio/198662>`_
+
+"""
+
+# imports
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/todbot/CircuitPython_TouchPIO.git"
+
+import array
+import rp2pio
+import adafruit_pioasm
+
+
+class TouchIn:
+    """
+    Read the state of a capacitive touch sensor.
+
+    Usage::
+
+      import touchpio
+      import board
+
+      touch = touchpio.TouchIn(board.GP2)
+      while True:
+          if touch.value:
+              print("touched!")
+
+    """
+
+    capsense_pio_code = adafruit_pioasm.assemble(
+    """
+    pull block           ; trigger a reading, get maxcount value from fifo, OSR contains maxcount
+    set pindirs, 1       ; set GPIO as output
+    set pins, 1          ; drive pin HIGH to charge capacitance
+;    set x,24             ; wait time for pin charge
+    set x,30             ; wait time for pin charge
+charge:                  ; wait (24+1)*31 = 1085 cycles = 8.6us
+    jmp x--, charge [31]
+    mov x, osr           ; load maxcount value (10_000 usually)
+    set pindirs, 0       ; set GPIO as input
+timing:
+    jmp x--, test        ; decrement x until timeout
+    jmp done             ; we've timed out, so leave
+test:
+    jmp pin, timing      ; loop while pin is still high
+done:
+    mov isr, x           ; load ISR with count value in x
+    push                 ; push ISR into RX fifo
+    """)
+
+    def __init__(self, touch_pin, max_count=10_000):
+        """Use the TouchIn on the given pin.
+
+        :param ~microcontroller.Pin pin: the pin to read from
+        """
+        self.max_count = max_count
+        self.pio = rp2pio.StateMachine( TouchIn.capsense_pio_code,
+                                        frequency=125_000_000,
+                                        first_set_pin = touch_pin,
+                                        jmp_pin = touch_pin )
+        self.max_count = 10_000
+        self.buf_send = array.array("L", [max_count] )   # 32-bit value
+        self.buf_recv = array.array("L", [0] )  # 32-bit value
+        self.base_val = self.raw_value
+        self.last_val = self.base_val
+        self.threshold = self.base_val + 200
+        if self.base_val == 0xffffffff:  # -1
+            raise ValueError("No pulldown on pin; 1Mohm recommended")
+
+    def _raw_read(self):
+        self.pio.write( self.buf_send )
+        self.pio.readinto( self.buf_recv )
+        v = self.buf_recv[0]   # return 32-bit number from PIO
+        if v > self.max_count: v = self.last_val
+        self.last_val = v
+        return v
+
+    @property
+    def raw_value(self):
+        """"The raw touch measurement as an `int`. (read-only)"""
+        return self.max_count - self._raw_read()
+
+    @property
+    def value(self):
+        """Whether the touch pad is being touched or not. (read-only)
+        True when `raw_value` > `threshold`."""
+        return self.raw_value > self.threshold
